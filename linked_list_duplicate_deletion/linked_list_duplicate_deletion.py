73f0509607bc2ff29e2e624b23a6f4beff02ceb6
Saadullah <aleemsaadullah@gmail.com>
2019-10-08 22:00:08 +0400

    deletion of duplicate nodes from linked list

---
+++ linked_list_duplicate_deletion/linked_list_duplicate_deletion.py
@@ -0,0 +1,51 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Solution:
+    def insert(self, head, data):
+        p = Node(data)
+        if head == None:
+            head = p
+        elif head.next == None:
+            head.next = p
+        else:
+            start = head
+            while (start.next != None):
+                start = start.next
+            start.next = p
+        return head
+
+    def display(self, head):
+        current = head
+        while current:
+            print(current.data, end=' ')
+            current = current.next
+
+    def removeDuplicates(self, head):
+        visited = set()
+
+        cur = head
+        prev = None
+        while cur:
+            if cur.data in visited:
+                prev.next = cur.next
+                # delete node
+            else:
+                visited.add(cur.data)
+                prev = cur
+            cur = cur.next
+
+        return head
+
+
+mylist = Solution()
+T = int(input())
+head = None
+for i in range(T):
+    data = int(input())
+    head = mylist.insert(head, data)
+head = mylist.removeDuplicates(head)
+mylist.display(head);
